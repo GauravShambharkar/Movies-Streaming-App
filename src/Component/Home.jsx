92b1e8c1f7bb46d8737ba0a3acf745af8deebd18
Gaurav Shambharkar <shambharkargaurav04@gmail.com>
2025-04-16 23:12:10 +0530

    init routes

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -4,10 +4,10 @@ const Home = () => {
   return (
     <>
       <div className="w-full h-full border p-4 mix-blend-difference backdrop-blur-sm  flex flex-col justify-center items-center gap-4" >
-            <h1 className="text-[#7499ff] p-4 rounded-2xl lg:text-7xl font-semibold text-center max-md:text-3xl max-sm:text-[30px]" > Your streaming guide for <br />  movies, TV shows & <br /> sports </h1>
+            <h1 className="text-[#7499ff] p-4 rounded-2xl lg:text-7xl font-semibold text-center max-md:text-3xl max-sm:text-[30px]" > Your Streaming Guide For <br />  Movies, TV Shows & <br /> Sports. </h1>
             <p className="text-[#bdbdbd] text-center">Find where to stream new, popular & upcoming entertainment with JustWatch.</p>
 
-        <button className="bg-[#7499ff] backdrop-blur-xl border border-white p-2 rounded-lg shadow-2xl shadow-[#ffe8e8] text-[#ffffff] cursor-pointer">Discove Movies & TV Shows</button>
+        <button className="bg-[#e9e9e9] backdrop-blur-xl border border-white p-2 rounded-lg shadow-2xl shadow-[#ffe8e8] text-[#4576ff] hover:bg-[#4576ff] hover:text-white transition-colors duration-200 ease-in-out cursor-pointer">Discover Movies & TV Shows</button>
       </div>
     </>
   );
--- src/Routing/Nav.jsx
+++ src/Routing/Nav.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import Routing from "./Routing";
-import { RiColorFilterAiLine, RiFile2Line, RiFilmLine, RiHomeWifiLine, RiMovie2Line } from "@remixicon/react";
+import { RiCloseCircleLine, RiCloseLargeLine, RiColorFilterAiLine, RiCrossLine, RiFile2Line, RiFilmLine, RiHomeWifiLine, RiMovie2Line, RiSearch2Line } from "@remixicon/react";
 
 const Nav = () => {
   return (
     <>
     {/* navBar */}
-    <div className="navbar w-full justify-between fixed z-1 gap-4 items-center px-2 border-r border-[#7d7d7d] bg-[#000000] backdrop-blur-lg flex pt-3">
+    <div className="navbar w-full justify-between fixed z-1 gap-4 items-center px-2 py-2 border-r border-b border-[#3f7cff]  bg-[#ffffff07] backdrop-blur-lg flex pt-3">
 
     {/* navBar left side */}
       <div className="text-[#4d7cff] items-center text-2xl p-2 flex">
@@ -15,26 +15,45 @@ const Nav = () => {
       <h1 className="font-black text-[#5381ff]">StreamUp</h1>
       </div>
 
-      {/* navBar right side */}
-      <div className="flex flex-row items-center gap-2 px-2 border border-white" >
 
-          <NavLink to='/' className={({isActive})=>{
-            return isActive? "text-[#ffffff] flex gap-2 " : "text-[#ffffff] flex gap-2"
-          }}>
-            <RiHomeWifiLine/>
-            Home</NavLink>
-          <NavLink to='/series' className={({isActive})=>{
-            return isActive? "text-[#ffffff] flex gap-2 " : "text-[#ffffff] flex gap-2"
-          }}  >
-            <RiColorFilterAiLine/>
-            Series</NavLink>
-          <NavLink to='/movies' className={({isActive})=>{
-            return isActive? "text-[#ffffff] flex gap-2 " : "text-[#ffffff] flex gap-2"
-          }}  >
-            <RiFilmLine/>
-            
-            Movies</NavLink>
+      {/* navBar right side */}
+      <div className="rightSIde flex w-full border-white justify-end ">
+        <div className="flex flex-row items-center gap-3 px-2  border-white" >
+
+            <NavLink to='/' className={({isActive})=>{
+              return isActive? "text-[#a2c9ff]  items-center flex gap-2 text-[13px] " : "text-[#ffffff] items-center text-[13px] flex gap-2"
+            }}>
+              Home</NavLink>
+            <NavLink to='/trending' className={({isActive})=>{
+              return isActive? "text-[#a2c9ff] flex gap-2 items-center text-[13px]" : "text-[#ffffff] items-center flex text-[13px] gap-2"
+            }}  >
+              Trending</NavLink>
+            <NavLink to='/popular' className={({isActive})=>{
+              return isActive? "text-[#a2c9ff] flex gap-2 items-center text-[13px]" : "text-[#ffffff] items-center flex text-[13px] gap-2"
+            }}  >
+              Popular</NavLink>
+            <NavLink to='/movies' className={({isActive})=>{
+              return isActive? "text-[#a2c9ff] flex gap-2 items-center text-[13px]" : "text-[#ffffff] items-center flex text-[13px] gap-2"
+            }}  >
+              Movies</NavLink>
+
+            <NavLink to='/tvshow' className={({isActive})=>{
+              return isActive? "text-[#a2c9ff] flex gap-2 items-center text-[13px]" : "text-[#ffffff] items-center flex text-[13px] gap-2"
+            }}  >
+              TVshow</NavLink>
+
+            </div>
+
+
+            <div className="input flex gap-2 w-[50%] p-2 rounded-sm ">
+            <RiSearch2Line className="text-white" />
+            <input type="text" placeholder="Search" className="border-b border-[#ffffff] text-white outline-none w-full" />
+            <RiCloseLargeLine className="cursor-pointer text-white" />
+            <button className="px-2 bg-[#578dff] text-[#ffffff] rounded-md" >Search</button>
+            </div>
       </div>
+
+
     </div>
 
 
