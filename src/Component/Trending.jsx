7d446ede1f9570c38d139113c639312dfa1dddd4
Gaurav Shambharkar <shambharkargaurav04@gmail.com>
2025-04-16 23:12:01 +0530

    added new route page

---
+++ src/Component/Trending.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Trending = () => {
+  return (
+    <div className="w-full h-full border p-4 mix-blend-difference backdrop-blur-sm  flex flex-col justify-center items-center gap-4" >
+    
+    </div>
+
+  )
+}
+
+export default Trending
