a5613952fb3a834e02aa8e49e1023041bb6a58e3
Gaurav Shambharkar <shambharkargaurav04@gmail.com>
2025-04-16 14:11:48 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Nav from "./Routing/Nav";
+
+function App() {
+  return (
+    <>
+      <div className="main w-[95%] mx-auto h-screen bg-[#121212] ">
+        <Nav />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Routing/Nav.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Routing from "./Routing";
+import { RiMovie2Line } from "@remixicon/react";
+
+const Nav = () => {
+  return (
+    <>
+    {/* navBar */}
+    <div className="navbar border border-white flex justify-between p-3 ">
+
+    {/* navBar left side */}
+      <div className="text-red-700">
+      <RiMovie2Line/>
+      </div>
+
+      {/* navBar right side */}
+      <div className="flex gap-4 " >
+
+          <NavLink to='/' className={({isActive})=>{
+            return isActive? "text-red-500" : "text-[#ffffff]"
+          }}  >Home</NavLink>
+          <NavLink to='/series' className={({isActive})=>{
+            return isActive? "text-red-500" : "text-[#ffffff]"
+          }}  >Series</NavLink>
+          <NavLink to='/movies' className={({isActive})=>{
+            return isActive? "text-red-500" : "text-[#ffffff]"
+          }}  >Movies</NavLink>
+      </div>
+    </div>
+
+
+
+      <Routing/>
+    </>
+  );
+};
+
+export default Nav;
