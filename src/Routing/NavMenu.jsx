2682d621cd378a2d4b0cd9ad8389fdef1d002f30
Gaurav Shambharkar <shambharkargaurav04@gmail.com>
2025-04-25 19:27:53 +0530

    navbar update

--- src/Routing/NavMenu.jsx
+++ src/Routing/NavMenu.jsx
@@ -1,35 +1,35 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const NavMenu = ({setShowNavMenu}) => {
+const NavMenu = () => {
   return (
-    <div className='w-full fixed bottom-0  rounded-full right-0 z-10 flex justify-center p-4 border-white transition-all duration-500 ease-in-out transform animate-fade-slide'>
-        <div className="border-white p-2 rounded-full bg-[#ffffff26] backdrop-blur-2xl">
+    <div className='w-full fixed bottom-0 right-0 z-10 flex justify-center p-3 border-white transition-all duration-500 ease-in-out transform animate-fade-slide'>
+        <div className="border-white w-full justify-center flex gap-3 p-3 rounded-full bg-[#ffffff26] backdrop-blur-2xl">
 
-        <div className='border-white w-fit flex gap-5' >
-        <NavLink onClick={()=>setShowNavMenu('')} to='/' className={({isActive})=>{
+        {/* <div className='border border-white w-fit flex gap-5 ' > */}
+        <NavLink  to='/' className={({isActive})=>{
               return isActive? "text-[#f7ff66]  items-center flex gap-2 text-[16px] " : "text-[#ffffff] hover:text-[#fbffb7] items-center text-[16px] flex gap-2"
             }}>
               Home</NavLink>
-            <NavLink onClick={()=>setShowNavMenu('')} to='/trending' className={({isActive})=>{
+            <NavLink  to='/trending' className={({isActive})=>{
               return isActive? "text-[#f7ff66] flex gap-2 items-center justify-end text-[16px]" : "text-[#ffffff] hover:text-[#fbffb7] items-center flex text-[16px] gap-2"
             }}  >
               Trending</NavLink>
-            <NavLink onClick={()=>setShowNavMenu('')} to='/popular' className={({isActive})=>{
+            <NavLink  to='/popular' className={({isActive})=>{
               return isActive? "text-[#f7ff66] flex gap-2 items-center text-[16px]" : "text-[#ffffff] hover:text-[#fbffb7] items-center flex text-[16px] gap-2"
             }}  >
               Popular</NavLink>
-            <NavLink onClick={()=>setShowNavMenu('')} to='/movies' className={({isActive})=>{
+            <NavLink  to='/movies' className={({isActive})=>{
               return isActive? "text-[#f7ff66] flex gap-2 items-center text-[16px]" : "text-[#ffffff] hover:text-[#fbffb7] items-center flex text-[16px] gap-2"
             }}  >
               Movies</NavLink>
 
-            <NavLink onClick={()=>setShowNavMenu('')} to='/tvshow' className={({isActive})=>{
+            <NavLink  to='/tvshow' className={({isActive})=>{
               return isActive? "text-[#f7ff66] flex gap-2 items-center text-[16px]" : "text-[#ffffff] hover:text-[#fbffb7] items-center flex text-[16px] gap-2"
             }}  >
               TVshow</NavLink>
             </div>
-        </div>
+        {/* </div> */}
         </div>
   )
 }
